1880ff4f94e902498be21cdc4cd60403c44fe5a4
eamon <eamon@EAMONNS-GALAXY->
2024-08-04 22:05:48 +0100

    Add project files.

---
+++ LeaveManagementSystem.web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using LeaveManagementSystem.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Eamonn"
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem.web/Models/TestViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LeaveManagementSystem.web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+    }
+}
