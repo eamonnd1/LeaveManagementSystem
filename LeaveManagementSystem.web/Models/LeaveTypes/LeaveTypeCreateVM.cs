c7b0886b8844bbe00035cc3f6ef5852e20e47db3
Eamonn <edelaney1992@gmail.com>
2024-09-15 14:20:24 +0100

    Add client side authentication on LeaveTypeCreateVM

--- LeaveManagementSystem.web/Models/LeaveTypes/LeaveTypeCreateVM.cs
+++ LeaveManagementSystem.web/Models/LeaveTypes/LeaveTypeCreateVM.cs
@@ -1,8 +1,15 @@
-﻿namespace LeaveManagementSystem.web.Models.LeaveTypes
+﻿using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.web.Models.LeaveTypes
 {
     public class LeaveTypeCreateVM
     {
+        [Required]
+        [Length(4, 150, ErrorMessage = "Enter a value between 4 and 150 chatacters")]
         public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(1, 90)]
         public int NumberOfDays { get; set; }
     }
 }
