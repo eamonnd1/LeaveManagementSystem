c55915a6f330e2577d39614f4fd912b292610865
Eamonn <edelaney1992@gmail.com>
2024-10-12 17:35:42 +0100

    added some logic to approval process

--- LeaveManagementSystem.web/Models/LeaveRequests/ReviewLeaveRequestVM.cs
+++ LeaveManagementSystem.web/Models/LeaveRequests/ReviewLeaveRequestVM.cs
@@ -1,7 +1,11 @@
-﻿namespace LeaveManagementSystem.web.Models.LeaveRequests;
+﻿using System.ComponentModel;
+
+namespace LeaveManagementSystem.web.Models.LeaveRequests;
 
 public class ReviewLeaveRequestVM : LeaveRequestReadOnlyVM
 {
     public EmployeeListVM Employee { get; set; } = new EmployeeListVM();
+
+    [DisplayName("Additional information")]
     public string RequestComments { get; set; }
 }
