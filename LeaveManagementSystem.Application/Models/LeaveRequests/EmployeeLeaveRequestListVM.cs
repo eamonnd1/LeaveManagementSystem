88eab635022afadb47b6200564a9a2d6ae35d8ac
Eamonn <edelaney1992@gmail.com>
2024-10-23 22:07:42 +0100

    added icons and some ui fixes

--- LeaveManagementSystem.Application/Models/LeaveRequests/EmployeeLeaveRequestListVM.cs
+++ LeaveManagementSystem.Application/Models/LeaveRequests/EmployeeLeaveRequestListVM.cs
@@ -2,16 +2,16 @@
 
 public class EmployeeLeaveRequestListVM
 {
-    [Display(Name = "Total number of requests")]
+    [Display(Name = "Total")]
     public int TotalRequests { get; set; }
 
-    [Display(Name = "Approved requests")]
+    [Display(Name = "Approved")]
     public int ApprovedRequests { get; set; }
 
-    [Display(Name = "Pending requests")]
+    [Display(Name = "Pending")]
     public int PendingRequests { get; set; }
 
-    [Display(Name = "Declined requests")]
+    [Display(Name = "Declined")]
     public int DeclinedRequests { get; set; }
 
     public List<LeaveRequestReadOnlyVM> LeaveRequests { get; set; } = [];
